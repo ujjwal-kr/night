8ffde60baa9fe22976bd38b7e96a75bef701510b
Ujjwal <epicujjwal@protonmail.com>
2022-04-07 10:53:48 +0530

    night_protocol: add Main to add more high level funcs

--- night_protocol/src/main.rs
+++ night_protocol/src/main.rs
@@ -2,12 +2,34 @@
 extern crate rocket;
 use sha2::{Digest, Sha256};
 
+#[derive(Debug)]
+struct Main {
+    pub blocks: Vec<Block>,
+}
+
 #[derive(Debug, Clone)]
 struct Block {
     block_data: String,
     previous_hash: String,
     transaction_list: Vec<String>,
     block_hash: String,
+}
+
+impl Main {
+    fn new() -> Self {
+        Self {
+            blocks: vec![Block::new(
+                "genesus".to_string(),
+                ["tran1".to_string()].to_vec(),
+            )],
+        }
+    }
+
+    pub fn add_block(&mut self, transactions: Vec<String>) {
+        let prev_block = &self.blocks[self.blocks.len() - 1];
+        let new_block = Block::new(prev_block.clone().block_hash, transactions);
+        self.blocks.push(new_block);
+    }
 }
 
 impl Block {
@@ -26,26 +48,17 @@ impl Block {
 
 #[launch]
 fn rocket() -> _ {
-    let genesus = Block::new(
-        "genesus".to_string(),
-        ["trans 1".to_string(), "trans 2".to_string()].to_vec(),
-    );
-
-    let mut blocks: Vec<Block> = vec![];
-    blocks.push(genesus);
-
-    add_block(["trans 3".to_string(), "trans 4".to_string()].to_vec(), &mut blocks);
-    add_block(["trans 5".to_string(), "trans 6".to_string()].to_vec(), &mut blocks);
-    add_block(["trans 7".to_string(), "trans 8".to_string()].to_vec(), &mut blocks);
+    let mut blocks = Main::new();
+    blocks.add_block(["tran2".to_string(), "tran3".to_string()].to_vec());
+    blocks.add_block(["tran4".to_string(), "tran5".to_string()].to_vec());
+    blocks.add_block(["tran6".to_string(), "tran7".to_string()].to_vec());
+    
+    for block in blocks.blocks {
+        println!("{:?} && {:?}", block.block_hash, block.previous_hash);
+    }
 
     //  server
     rocket::build().mount("/", routes![index])
-}
-
-fn add_block(transactions: Vec<String>, blocks: &mut Vec<Block>) {
-    let prev_block = &blocks[blocks.len() -1];
-    let new_block = Block::new(prev_block.clone().block_hash, transactions);
-    blocks.push(new_block);
 }
 
 #[get("/")]
