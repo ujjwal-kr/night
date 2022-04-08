52972fd33bded96f6c693d665624666313efe5f2
Ujjwal <epicujjwal@protonmail.com>
2022-04-08 22:09:39 +0530

    night_prtcl: separate blockchain structs and prettify prints

---
+++ night_protocol/src/blockchain/blockchain.rs
@@ -0,0 +1,101 @@
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+
+#[derive(Debug)]
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Block {
+    pub id: i64,
+    pub previous_hash: String,
+    pub transaction: Transaction,
+    pub block_hash: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Transaction {
+    pub sender: String,
+    pub reciever: String,
+    pub amount: f64,
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Self {
+            blocks: vec![Block::new(
+                0,
+                "genesus".to_string(),
+                Transaction {
+                    sender: "test".to_string(),
+                    reciever: "net".to_string(),
+                    amount: 500.0,
+                },
+            )],
+        }
+    }
+
+    pub fn add_block(&mut self, transactions: Transaction) {
+        let prev_block = &self.blocks[self.blocks.len() - 1];
+        let new_block = Block::new(
+            prev_block.id + 1,
+            prev_block.clone().block_hash,
+            transactions,
+        );
+        self.blocks.push(new_block);
+    }
+
+    pub fn validate_block(&self, block: &Block, previous_block: &Block) -> bool {
+        if previous_block.block_hash.trim() == block.previous_hash.trim() {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn validate_chain(&self) {
+        for i in 1..self.blocks.len() {
+            let block = &self.blocks[i];
+            let prev_block = &self.blocks[i - 1].clone();
+            if self.validate_block(block, prev_block) == false {
+                println!("block bad");
+                return;
+            }
+        }
+        println!("blocks good");
+    }
+
+    pub fn find_block_by_hash(&self, hash: String) -> Block {
+        for block in self.blocks.clone() {
+            if block.block_hash.trim() == hash.trim() {
+                return block;
+            }
+        }
+
+        Block {
+            id: 0,
+            block_hash: "null".to_string(),
+            previous_hash: "null".to_string(),
+            transaction: Transaction {
+                sender: "null".to_string(),
+                reciever: "null".to_string(),
+                amount: 0.00,
+            },
+        }
+    }
+}
+
+impl Block {
+    pub fn new(id: i64, previous_hash: String, transaction: Transaction) -> Block {
+        let mut hasher = Sha256::new();
+        hasher.update(format!("{}-{}-{:?}", id, previous_hash, transaction));
+        let block_hash_str: String = format!("{:x}", hasher.finalize());
+        Block {
+            id: id,
+            block_hash: block_hash_str,
+            previous_hash: previous_hash,
+            transaction: transaction,
+        }
+    }
+}
--- night_protocol/src/main.rs
+++ night_protocol/src/main.rs
@@ -1,106 +1,7 @@
 #[macro_use]
 extern crate rocket;
-use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
-
-#[derive(Debug)]
-struct Blockchain {
-    pub blocks: Vec<Block>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Block {
-    id: i64,
-    previous_hash: String,
-    transaction: Transaction,
-    block_hash: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Transaction {
-    sender: String,
-    reciever: String,
-    amount: f64,
-}
-
-impl Blockchain {
-    fn new() -> Self {
-        Self {
-            blocks: vec![Block::new(
-                0,
-                "genesus".to_string(),
-                Transaction {
-                    sender: "test".to_string(),
-                    reciever: "net".to_string(),
-                    amount: 500.0,
-                },
-            )],
-        }
-    }
-
-    pub fn add_block(&mut self, transactions: Transaction) {
-        let prev_block = &self.blocks[self.blocks.len() - 1];
-        let new_block = Block::new(
-            prev_block.id + 1,
-            prev_block.clone().block_hash,
-            transactions,
-        );
-        self.blocks.push(new_block);
-    }
-
-    fn validate_block(&self, block: &Block, previous_block: &Block) -> bool {
-        if previous_block.block_hash.trim() == block.previous_hash.trim() {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn validate_chain(&self) {
-        for i in 1..self.blocks.len() {
-            let block = &self.blocks[i];
-            let prev_block = &self.blocks[i - 1].clone();
-            if self.validate_block(block, prev_block) == false {
-                println!("block bad");
-                return;
-            }
-        }
-        println!("blocks good");
-    }
-
-    fn find_block_by_hash(&self, hash: String) -> Block {
-        for block in self.blocks.clone() {
-            if block.block_hash.trim() == hash.trim() {
-                return block;
-            }
-        }
-
-        Block {
-            id: 0,
-            block_hash: "null".to_string(),
-            previous_hash: "null".to_string(),
-            transaction: Transaction {
-                sender: "null".to_string(),
-                reciever: "null".to_string(),
-                amount: 0.00,
-            },
-        }
-    }
-}
-
-impl Block {
-    fn new(id: i64, previous_hash: String, transaction: Transaction) -> Block {
-        let mut hasher = Sha256::new();
-        hasher.update(format!("{}-{}-{:?}", id, previous_hash, transaction));
-        let block_hash_str: String = format!("{:x}", hasher.finalize());
-        Block {
-            id: id,
-            block_hash: block_hash_str,
-            previous_hash: previous_hash,
-            transaction: transaction,
-        }
-    }
-}
+mod blockchain;
+use blockchain::blockchain::*;
 
 fn main() {
     let mut blocks = Blockchain::new();
@@ -115,25 +16,30 @@ fn main() {
             reciever: "net".to_string(),
             amount: i * 5.0,
         });
-        if i == 10.0 {
+        if i == 3.0 {
             break;
         }
     }
 
     // Example Actions
 
+    // print all the blocks
+
     for block in &blocks.blocks {
-        println!("{:?} \n", block);
+        print!(
+            "id:{}\n hash: {},\n previous_hash: {},\n Transaction: {:?}\n\n",
+            block.id, block.block_hash, block.previous_hash, block.transaction
+        );
     }
 
-    // print all the blocks
+    // validate the blockchain
 
     blocks.validate_chain();
 
     // Finding a block by known hash
 
     let my_block = blocks.find_block_by_hash(
-        "55f2610a693e8babf9f828a6f68af6e8567e601bf32153550dc811d757ff8c91".to_string(),
+        "aa89390f927d1c05d70252caf9963e2c82c1fe153c906afe9fb51f0e3cd8c203".to_string(),
     );
 
     println!("{:?}", my_block);
