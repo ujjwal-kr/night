32bc24b19beba6b9e88df430f70b99a1bd399015
Ujjwal <epicujjwal@protonmail.com>
2022-04-10 08:52:28 +0530

    night_prctl: add global hash find func for both chains

--- night_protocol/src/main.rs
+++ night_protocol/src/main.rs
@@ -18,7 +18,7 @@ fn rocket() -> _ {
 
     loop {
         i = i + 1.0;
-        if blocks.blocks.len() == 10 {
+        if blocks.blocks.len() == 30 {
             master_blocks.validate_chain();
             blocks.validate_chain();
             master_blocks.add_master_block(blocks.blocks);
@@ -40,12 +40,12 @@ fn rocket() -> _ {
 
     // print all the blocks
 
-    // for block in &blocks.blocks {
-    //     print!(
-    //         "id:{}\n hash: {},\n previous_hash: {},\n Transaction: {:?}\n\n",
-    //         block.id, block.block_hash, block.previous_hash, block.transaction
-    //     );
-    // }
+    for block in &blocks.blocks {
+        print!(
+            "id:{}\n hash: {},\n previous_hash: {},\n Transaction: {:?}\n\n",
+            block.id, block.block_hash, block.previous_hash, block.transaction
+        );
+    }
 
     // print the master blockchain
 
@@ -58,20 +58,6 @@ fn rocket() -> _ {
             master_block.block_data
         )
     }
-
-    // Finding a block by known hash
-
-    // let my_block = blocks.find_block_by_hash(
-    //     "aa89390f927d1c05d70252caf9963e2c82c1fe153c906afe9fb51f0e3cd8c203".to_string(),
-    // );
-
-    // println!("{:?}", my_block);
-
-    let my_block = master_blocks.find_block_by_hash(
-        "ea678fa3238eb37fdf93a4da83bfba13969ff79e8e563925242813d9635dd94b".to_string(),
-    );
-
-    println!("{:?}", my_block);
 
     //  server
     rocket::build()
@@ -98,10 +84,24 @@ fn get_block(hash: String, blocks: &State<Blockchain>) -> Json<String> {
 }
 
 // Find Transaction
-
 #[get("/<hash>")]
-fn get_master_block_block(hash: String, master_blocks: &State<Master>) -> Json<String> {
-    let block: Block = master_blocks.find_block_by_hash(hash);
+fn get_master_block_block(hash: String, master_blocks: &State<Master>, blocks: &State<Blockchain>) -> Json<String> {
+    let block: Block;
+    let possible_master_block = master_blocks.find_block_by_hash(hash.clone());
+    let possible_block = blocks.find_block_by_hash(hash);
+
+    if possible_block.id != 0 {
+        block = possible_block;
+        let serialized = serde_json::to_string(&block).unwrap();
+        return Json(serialized);
+    } else if possible_master_block.id != 0 {
+        block = possible_master_block;
+        let serialized = serde_json::to_string(&block).unwrap();
+        return Json(serialized);
+    } else {
+        block = possible_block;
+    }
+
     let serialized = serde_json::to_string(&block).unwrap();
     Json(serialized)
 }
