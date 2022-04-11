ac65da8961b832098f65f25b38159903c1093247
Ujjwal <epicujjwal@protonmail.com>
2022-04-11 17:25:33 +0530

    client: add particles

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { Text, Title, Button } from "@mantine/core";
+import Particles from "react-tsparticles";
 
 const Home: NextPage = () => {
+  return (
+    <>
+      <Top_Container />
+    </>
+  );
+};
+
+const Top_Container = () => {
   return (
     <>
       <div className={styles.top_container}>
@@ -14,9 +23,53 @@ const Home: NextPage = () => {
           <br />
           <Button className={styles.text} variant="gradient" gradient={{ from: 'orange', to: 'red' }} >Dashboard</Button>
         </div>
+        
+        <Particles
+          params={{
+            "particles": {
+              "number": {
+                "value": 160,
+                "density": {
+                  "enable": true,
+                  "value_area": 1500
+                }
+              },
+              "line_linked": {
+                "enable": true,
+                "opacity": 0.02
+              },
+              "move": {
+                "direction": "right",
+                "speed": 2
+              },
+              "size": {
+                "value": 2
+              },
+              "opacity": {
+                "anim": {
+                  "enable": true,
+                  "speed": 1,
+                  "opacity_min": 0.05
+                }
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onclick": {
+                  "enable": false,
+                }
+              },
+              "modes": {
+                "push": {
+                  "particles_nb": 1
+                }
+              }
+            },
+            "retina_detect": true
+          }} />
       </div>
     </>
-  );
-};
+  )
+}
 
 export default Home;
