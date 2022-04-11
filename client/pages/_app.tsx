7e3caa2c9f36caab50b3c1f9602a54f7aa502d39
shivam danawale <shivamdanawale@gmail.com>
2022-04-11 12:09:21 +0530

    :rocket: Made home page for night protocol

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, NextUIProvider, Text } from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: "$green200",
+      primary: "#4ADE7B",
+      primaryDark: "$green600",
+      darkBg: "#121212",
+      headBg: "#101429",
+      headText: "#e8ebe9",
+      subText: "#d9dbda",
+    },
+    space: {},
+    fonts: {},
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,48 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { Button, Card, Container, Spacer, Text } from "@nextui-org/react";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <>
+      <Container
+        css={{
+          background: "$darkBg",
+          height: "100vh",
+          p: "0",
+          m: "0",
+          width: "100%",
+          color: "$subText",
+        }}
+        className={styles.text}
+      >
+        <Card
+          css={{
+            background: "$headBg",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "70%",
+            width: "100%",
+          }}
+          className={styles.headCard}
+        >
+          <Text
+            size={40}
+            css={{ fontWeight: 700, color: "$headText" }}
+            className={styles.text}
+          >
+            Night Protocol
+          </Text>
+          A blockchain protocol written in Rust
+          <Spacer y={1} />
+          <Button color="gradient" auto shadow size="lg">
+            Get started{" "}
+          </Button>
+        </Card>
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
