4e271fa891e0cc5bec7a1b257834f4fff9fe1b4c
Ujjwal <epicujjwal@protonmail.com>
2022-04-14 06:40:16 +0530

    add CORS

--- night_protocol/src/main.rs
+++ night_protocol/src/main.rs
@@ -11,6 +11,32 @@ mod blockchain;
 use blockchain::blockchain::*;
 use blockchain::master_chain::*;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 struct SharedBlockchain {
     blocks: Mutex<Blockchain>,
@@ -24,7 +50,7 @@ struct SharedMaster {
 #[launch]
 fn rocket() -> _ {
     let mut blocks = Blockchain::new();
-    let mut master_blocks = Master::new();
+    let master_blocks = Master::new();
 
     let mut i = 0.0f64;
 
@@ -75,6 +101,7 @@ fn rocket() -> _ {
 
     //  server
     rocket::build()
+        .attach(CORS)
         .manage(SharedBlockchain {
             blocks: Mutex::new(blocks),
         })
@@ -140,12 +167,15 @@ fn get_transaction(
     shared_master_blocks: &State<SharedMaster>,
     shared_blocks: &State<SharedBlockchain>,
 ) -> Json<String> {
-
     let blocks: &SharedBlockchain = shared_blocks.inner();
     let master_blocks: &SharedMaster = shared_master_blocks.inner();
 
     let block: Block;
-    let possible_master_block = master_blocks.master_blocks.lock().unwrap().find_block_by_hash(hash.clone());
+    let possible_master_block = master_blocks
+        .master_blocks
+        .lock()
+        .unwrap()
+        .find_block_by_hash(hash.clone());
     let possible_block = blocks.blocks.lock().unwrap().find_block_by_hash(hash);
 
     if possible_block.id != 0 {
@@ -197,7 +227,11 @@ fn gamble(
         }
 
         if blocks.blocks.lock().unwrap().blocks.len() == 20 {
-            master_blocks.master_blocks.lock().unwrap().add_master_block(blocks.blocks.lock().unwrap().blocks.clone());
+            master_blocks
+                .master_blocks
+                .lock()
+                .unwrap()
+                .add_master_block(blocks.blocks.lock().unwrap().blocks.clone());
             blocks.blocks.lock().unwrap().blocks = vec![];
             blocks.blocks.lock().unwrap().genesus();
         }
