1d0e0d1420cc89a754ed0d55e1dc6036ee3c7383
Gurpal Singh <76196237+PhantomKnight287@users.noreply.github.com>
2022-04-16 14:08:27 +0530

    Add notifications  (#6)
    * add `@mantine/notifications` package
    
    * add notifications provider
    
    * format `dashboard.tsx` and show notifications
    
    * add `Head` in `index.tsx`

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,113 +1,159 @@
-import { Center, Container, TextInput, Button, Title, Table, Pagination, Alert } from "@mantine/core"
-import { NextPage } from "next"
-import { useEffect, useState } from "react"
-import Service from "../services/transaction"
-import { Block } from "../types/block"
-import { GambleData } from "../types/gamble"
-import styles from "../styles/Dashboard.module.css"
-
+import {
+  Center,
+  Container,
+  TextInput,
+  Button,
+  Title,
+  Table,
+  Pagination,
+  Alert,
+} from "@mantine/core";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Service from "../services/transaction";
+import { Block } from "../types/block";
+import { GambleData } from "../types/gamble";
+import styles from "../styles/Dashboard.module.css";
+import { useNotifications } from "@mantine/notifications";
 const GambleForm = () => {
-    const [value, setVal]: [string, Function] = useState('')
-    const [balance, setBalance]: [Number, Function] = useState(0)
-    useEffect(() => {
-        getBlanance()
-    }, [])
-    const getBlanance = async () => {
-        let balanceData = await Service.getBlanance()
-        setBalance(parseInt(balanceData.balance))
+  const [value, setVal]: [string, Function] = useState("");
+  const [balance, setBalance]: [Number, Function] = useState(0);
+  const { showNotification } = useNotifications();
+  useEffect(() => {
+    getBlanance();
+  }, []);
+  const getBlanance = async () => {
+    let balanceData = await Service.getBlanance();
+    setBalance(parseInt(balanceData.balance));
+  };
+  const gamble = async () => {
+    if (!isNaN(parseInt(value))) {
+      let res: GambleData = await Service.gamble(parseInt(value));
+      if (res.win == "true") {
+        showNotification({
+          message: "You won",
+          color: "green",
+        });
+      } else {
+        showNotification({
+          message: "You lost",
+          color: "red",
+        });
+      }
+      setBalance(res.newBalance);
+    } else {
+      alert("Wrong Gamble amount");
     }
-    const gamble = async () => {
-        if (!isNaN(parseInt(value))) {
-            let res: GambleData = await Service.gamble(parseInt(value))
-            setBalance(res.newBalance)
-        } else {
-            alert("Wrong Gamble amount")
-        }
-    }
-    return (
-        <>
-            <Title className={styles.text} order={3}>Balance: {balance.toString()}</Title>
-            <br />
-            <TextInput
-                placeholder="Amount"
-                label="Gamble with Night Coins"
-                value={value}
-                onChange={(e) => setVal(e.currentTarget.value)}
-                required
-            />
-            <br />
-            <Button onClick={gamble} className={styles.text} variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }}>Gamble</Button>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <Title className={styles.text} order={3}>
+        Balance: {balance.toString()}
+      </Title>
+      <br />
+      <TextInput
+        placeholder="Amount"
+        label="Gamble with Night Coins"
+        value={value}
+        onChange={(e) => setVal(e.currentTarget.value)}
+        required
+      />
+      <br />
+      <Button
+        onClick={gamble}
+        className={styles.text}
+        variant="gradient"
+        gradient={{ from: "teal", to: "blue", deg: 60 }}
+      >
+        Gamble
+      </Button>
+    </>
+  );
+};
 
 const TransactionComponent = () => {
-    let [dataLength, setDataLength]: [number, Function] = useState(0)
-    let [page, setPage] = useState(1)
-    let [transactions, setTransaction]: [Block[], Function] = useState([])
-    useEffect(() => {
-        getTransactions()
-    }, [page])
-    useEffect(() => {
-        getDataLength()
-    }, [])
-    const refresh = () => {
-        getDataLength()
-        getTransactions()
-    }
-    const getTransactions = async () => {
-        let elements: Block[] = await Service.getTransactions(dataLength + 1 - page)
-        setTransaction(elements)
-    }
-    const getDataLength = async () => {
-        let dataLength = await Service.countDataLength()
-        setDataLength(dataLength)
-    }
-    const rows = transactions.map((element) => (
-        <tr key={element.id.toString()}>
-            <td>{element.id.toString()}</td>
-            <td>{element.transaction.sender}</td>
-            <td>{element.transaction.reciever}</td>
-            <td>${element.transaction.amount.toString()}</td>
-        </tr>
-    ))
-    return (
-        <div>
-            <h1 style={{ marginTop: 5 + `rem` }} className={styles.text}>Transactions</h1>
-            <Button className={styles.text} variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }} onClick={refresh} >Refresh</Button>
-            <br />
-            <br />
-            <Table className={styles.text}>
-                <thead>
-                    <tr >
-                        <th>ID</th>
-                        <th>Sender</th>
-                        <th>Reciever</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </Table>
-            <br />
-            <br />
-            <Center>
-                <Pagination onChange={setPage} total={dataLength} siblings={1} initialPage={1} />
-            </Center>
-            <br />
-        </div>
-    )
-}
+  let [dataLength, setDataLength]: [number, Function] = useState(0);
+  let [page, setPage] = useState(1);
+  let [transactions, setTransaction]: [Block[], Function] = useState([]);
+  useEffect(() => {
+    getTransactions();
+  }, [page]);
+  useEffect(() => {
+    getDataLength();
+  }, []);
+  const refresh = () => {
+    getDataLength();
+    getTransactions();
+  };
+  const getTransactions = async () => {
+    let elements: Block[] = await Service.getTransactions(
+      dataLength + 1 - page
+    );
+    setTransaction(elements);
+  };
+  const getDataLength = async () => {
+    let dataLength = await Service.countDataLength();
+    setDataLength(dataLength);
+  };
+  const rows = transactions.map((element) => (
+    <tr key={element.id.toString()}>
+      <td>{element.id.toString()}</td>
+      <td>{element.transaction.sender}</td>
+      <td>{element.transaction.reciever}</td>
+      <td>${element.transaction.amount.toString()}</td>
+    </tr>
+  ));
+  return (
+    <div>
+      <h1 style={{ marginTop: 5 + `rem` }} className={styles.text}>
+        Transactions
+      </h1>
+      <Button
+        className={styles.text}
+        variant="gradient"
+        gradient={{ from: "teal", to: "blue", deg: 60 }}
+        onClick={refresh}
+      >
+        Refresh
+      </Button>
+      <br />
+      <br />
+      <Table className={styles.text}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Sender</th>
+            <th>Reciever</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+      <br />
+      <br />
+      <Center>
+        <Pagination
+          onChange={setPage}
+          total={dataLength}
+          siblings={1}
+          initialPage={1}
+        />
+      </Center>
+      <br />
+    </div>
+  );
+};
 const Dashboard: NextPage = () => {
-    return (
-        <Center>
-            <div className={styles.container}>
-                <Container>
-                    <h1 className={styles.heading}>Dashboard</h1>
-                    <GambleForm />
-                    <TransactionComponent />
-                </Container>
-            </div>
-        </Center>
-    )
-}
-export default Dashboard
+  return (
+    <Center>
+      <div className={styles.container}>
+        <Container>
+          <h1 className={styles.heading}>Dashboard</h1>
+          <GambleForm />
+          <TransactionComponent />
+        </Container>
+      </div>
+    </Center>
+  );
+};
+export default Dashboard;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,63 +2,83 @@ import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { Button } from "@mantine/core";
 import Particles from "react-tsparticles";
-
+import Head from "next/head";
 const Home: NextPage = () => {
-
   return (
     <>
+      <Head>
+        <title>Night Protocol</title>
+        <meta
+          name="description"
+          content="A minimal blockchain protocol written in Rust for beginners to understand the modern infrastructure of blockchain."
+        />
+        <meta property="og:title" content="Night Protocol" />
+        <meta
+          property="og:description"
+          content="A minimal blockchain protocol written in Rust for beginners to understand the modern infrastructure of blockchain."
+        />
+      </Head>
       <div className={styles.top_container}>
         <div>
-          <h1 className={styles.heading}>
-            Night Protocol
-          </h1>
-          <p className={styles.description}>A minimal blockchain protocol written in Rust for beginners to understand <br /> the modern infrastructure of blockchain.</p>
-          <Button size="lg" className={styles.text} variant="gradient" gradient={{ from: 'orange', to: 'red' }} >Explore</Button>
+          <h1 className={styles.heading}>Night Protocol</h1>
+          <p className={styles.description}>
+            A minimal blockchain protocol written in Rust for beginners to
+            understand <br /> the modern infrastructure of blockchain.
+          </p>
+          <Button
+            size="lg"
+            className={styles.text}
+            variant="gradient"
+            gradient={{ from: "orange", to: "red" }}
+          >
+            Explore
+          </Button>
         </div>
 
         <Particles
           params={{
-            "particles": {
-              "number": {
-                "value": 140,
-                "density": {
-                  "enable": true,
-                  "value_area": 1500
-                }
+            particles: {
+              number: {
+                value: 140,
+                density: {
+                  enable: true,
+                  value_area: 1500,
+                },
               },
-              "line_linked": {
-                "enable": true,
-                "opacity": 0.02
+              line_linked: {
+                enable: true,
+                opacity: 0.02,
               },
-              "move": {
-                "direction": "right",
-                "speed": 2
+              move: {
+                direction: "right",
+                speed: 2,
               },
-              "size": {
-                "value": 2
+              size: {
+                value: 2,
               },
-              "opacity": {
-                "anim": {
-                  "enable": true,
-                  "speed": 1,
-                  "opacity_min": 0.05
-                }
-              }
+              opacity: {
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.05,
+                },
+              },
             },
-            "interactivity": {
-              "events": {
-                "onclick": {
-                  "enable": false,
-                }
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: false,
+                },
               },
-              "modes": {
-                "push": {
-                  "particles_nb": 1
-                }
-              }
+              modes: {
+                push: {
+                  particles_nb: 1,
+                },
+              },
             },
-            "retina_detect": true
-          }} />
+            retina_detect: true,
+          }}
+        />
       </div>
     </>
   );
