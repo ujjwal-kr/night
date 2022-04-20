f06f46ff102c2bd4e39c8738cd847ea162d17186
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-20 17:03:14 +0530

    add  and

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withMDX({
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  reactStrictMode:true
+});
